b662c6efbbe5517c956509096f2632af5e582dda
Ezra Velazquez <ezra.ev@gmail.com>
2013-09-21 15:02:20 -0400

    Fix bug to have replacement image mici current images size, along with js code clean

--- content.js
+++ content.js
@@ -1,5 +1,3 @@
-//var $pp = jQuery.noConflict(true);
-
 chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
     switch(message.type) {
         case "colors-div":
@@ -7,28 +5,31 @@ chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
             if(divs.length === 0) {
                 alert("There are no any divs in the page.");
             } else {
-            	/*
-            	var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=callback?";
-            	$pp.getJSON(flickerAPI,{
-            		tags: "puppies",
-            		tagmode: "any",
-            		format: "json"
-            	})
-            	.done(function(data){
-            		alert("hi");
-            		console.log(data);
-            	});
-            	*/
             	console.log("hello world");
             	var xhr = new XMLHttpRequest();
-				var flickrAPI = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=fba3eb503e17a328e0304c4ddcfcfe45&tags=puppy%2Cpuppies&format=json&nojsoncallback=1";
-				/*
-				var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?tags=puppy&tagmode=any&format=json"*/
-				
-				xhr.open("GET", flickrAPI, true);
+            	
+            	var apiParameters = {
+            		key            : '2476f0b870fe91f962ad80df0865ca0b',
+            		url            : 'http://api.flickr.com/services/rest/',
+            		method         : 'flickr.photos.search',
+            		tags           : 'cute%2Cyoung%2Cpuppy',
+            		tagMode        : 'all',
+            		format         : 'json',
+            		noJsonCallback : '1',
+            		photosPerPage  : 100,
+            		page           : 1
+            	}
+            	
+            	if(divs.length < 500){
+            		apiParameters.photosPerPage = divs.length;
+            	}
+            	
+            	var flickrAPICall = apiParameters.url + '?method=' + apiParameters.method + '&api_key=' + apiParameters.key + '&tags=' + apiParameters.tags + '&tag_mode=' + apiParameters.tagMode + '&per_page=' + apiParameters.photosPerPage + '&page' + apiParameters.page + '&format=' + apiParameters.format + '&nojsoncallback=' + apiParameters.noJsonCallback;
+            	//console.log(flickrAPICall);
+            	
+				xhr.open("GET", flickrAPICall, true);
 				xhr.onreadystatechange = function() {
 				  if (xhr.readyState == 4) {
-				    // JSON.parse does not evaluate the attacker's scripts.
 				    var resp = JSON.parse(xhr.responseText);
 				    console.log(resp);
 				    var imgCounter = 0;
@@ -42,11 +43,14 @@ chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
 				    	id = base.id;
 				    	secret = base.secret;
 				    	url = "http://farm"+farmId+".staticflickr.com/"+serverId+"/"+id+"_"+secret+".jpg";
-				    	//puppyID = resp.photos.photo[i].id;
-				    	//puppyOwner = resp.photos.photo[i].owner;
-				    	//puppyURL = "http://flickr.com/photos/"+puppyOwner+"/"+puppyID;
-				    	divs[i].setAttribute("src",url);
-				    	console.log(url);
+				    	// Get current dimensions being shown
+				    	currentHeight = divs[i].clientHeight;
+				    	currentWidth = divs[i].clientWidth;
+				    	
+				    	divs[i].setAttribute("src", url);
+				    	divs[i].setAttribute("width", currentWidth);
+				    	divs[i].setAttribute("height", currentHeight);
+				    	//console.log(url);
 				    	imgCounter += 1;
 				    }
 				  }
